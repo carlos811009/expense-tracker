a92ead33c09e956954ad972ac2d627b2e5701488
Carlos <carlos811009@gmail.com>
2021-06-06 17:01:03 +0800

    add authentication feature

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@ const home = require('./modules/home')
 const user = require('./modules/user')
 const expense = require('./modules/expenseUse')
 const methodOverride = require('method-override')
+const { authenticator } = require('../middleware/auth')//回傳的為物件
 
 router.use(methodOverride('_method'))
 
-router.use('/expense', expense)
+router.use('/expense', authenticator, expense)
 router.use('/users', user)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
